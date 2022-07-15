b1c47eb65e88bd89aa6f3190c306ba2588c9a8fc
lpenza <105391483+lpenza@users.noreply.github.com>
2022-07-15 00:18:51 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ do {
         alumnos.push(nombreAlumno)
     }else{
         validador = false
-        console.log("Los alumnos ingresados son")
-        for (let i = 0; i < alumnos.length; i++) {
-            console.log(`${i+1}-${alumnos[i]} `)
-        }
     }
 } while (validador)
+
+
+
+console.log("Los alumnos ingresados son")
+for (let i = 0; i < alumnos.length; i++) {
+    console.log(`${i+1}-${alumnos[i]} `)
+}
